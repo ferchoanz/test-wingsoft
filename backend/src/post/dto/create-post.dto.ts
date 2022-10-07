7c260819b70fc1ed718f27407d283e6ba4a50f26
ferchoanz <fercho0281@gmail.com>
2022-10-07 17:29:39 -0400

    validation not empty in the create post

--- backend/src/post/dto/create-post.dto.ts
+++ backend/src/post/dto/create-post.dto.ts
@@ -1,16 +1,20 @@
-import { IsString } from "class-validator";
+import { IsString, IsNotEmpty } from "class-validator";
 
 export class CreatePostDto {
-
+    
+    @IsNotEmpty()
     @IsString()
     title: string;
 
+    @IsNotEmpty()
     @IsString()
     summary: string;
 
+    @IsNotEmpty()
     @IsString()
     content: string
 
+    @IsNotEmpty()
     @IsString()
     author: string;
 }
